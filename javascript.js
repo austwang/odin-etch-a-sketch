56772dc32f4b5301140e402512e83ae166302137
Austin Wang <113540229+austwang@users.noreply.github.com>
2025-09-18 16:38:46 -0400

    change grid color when hovering over it with the mouse

--- javascript.js
+++ javascript.js
@@ -6,6 +6,9 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const element = document.createElement("div");
         element.classList.add("gridElement");
+        element.addEventListener("mouseover", () => {
+            element.style.backgroundColor = "black";
+        });
         row.appendChild(element);
     }
     container.appendChild(row);
