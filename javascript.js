ad6556b1e8dc49cbfd4b524f5efb1a658ae3598b
Austin Wang <113540229+austwang@users.noreply.github.com>
2025-09-18 16:47:19 -0400

    convert grid generation into function

--- javascript.js
+++ javascript.js
@@ -1,17 +1,19 @@
 container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("gridRow");
-    for (let j = 0; j < 16; j++) {
-        const element = document.createElement("div");
-        element.classList.add("gridElement");
-        element.addEventListener("mouseover", () => {
-            element.style.backgroundColor = "black";
-        });
-        row.appendChild(element);
+function generateGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.classList.add("gridRow");
+        for (let j = 0; j < gridSize; j++) {
+            const element = document.createElement("div");
+            element.classList.add("gridElement");
+            element.addEventListener("mouseover", () => {
+                element.style.backgroundColor = "black";
+            });
+            row.appendChild(element);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
 
 button = document.querySelector("button");
@@ -19,3 +21,5 @@ button = document.querySelector("button");
 button.addEventListener("click", () => {
     const gridSize = prompt("Enter the number of squares per side for the new grid", 16);
 });
+
+generateGrid(16);
